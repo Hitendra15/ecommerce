88b2bd9b2a90f5e57f431c5f994b717da68008e2
Hitendra15 <hitendramarathe15@gmail.com>
2026-04-29 14:34:59 +0530

    header search functionality

--- product/views.py
+++ product/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Product
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 # Create your views here.
 def index(request):
-    products = Product.objects.all().order_by('id')
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query)).order_by('id')
+    else:    
+        products = Product.objects.all().order_by('id')
     return render(request,'product/index.html',{'products':products})
 
 @login_required
